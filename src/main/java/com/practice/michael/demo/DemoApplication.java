275d7857b6fedc11b6461de5b7cc47ce948955ec
MSBauer <michaelxbauer@gmail.com>
2018-03-21 11:34:59 -0400

    application.properties fixed, component scan alternative lacks hibernate settings, currently working on 'clob not implemented' issue with the postgres jdbc driver

--- src/main/java/com/practice/michael/demo/DemoApplication.java
+++ src/main/java/com/practice/michael/demo/DemoApplication.java
@@ -1,11 +1,16 @@
 package com.practice.michael.demo;
 
+import com.practice.michael.demo.Datasources.DBPostgresConfigAlternative;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+		@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, value=DBPostgresConfigAlternative.class)
+})
 public class DemoApplication {
-
 	public static void main(String[] args){
 		SpringApplication.run(DemoApplication.class, args);
 	}
