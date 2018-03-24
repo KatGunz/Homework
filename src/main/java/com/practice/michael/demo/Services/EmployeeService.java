4f4a3db06760f9ee8bdbf7e2e8cadf483221eb65
MSBauer <michaelxbauer@gmail.com>
2018-03-23 22:04:58 -0400

    added @Transactional to EmployeeService

--- src/main/java/com/practice/michael/demo/Services/EmployeeService.java
+++ src/main/java/com/practice/michael/demo/Services/EmployeeService.java
@@ -5,7 +5,10 @@ import com.practice.michael.demo.DTOs.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
+@Transactional
 public class EmployeeService {
 
     @Autowired
