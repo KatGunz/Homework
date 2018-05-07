9eda181453280cba52824665e30c1640306f5494
MSBauer <michaelxbauer@gmail.com>
2018-05-06 21:22:30 -0400

    fixin things

--- src/main/java/com/practice/michael/demo/DAOs/EmployeeDao.java
+++ src/main/java/com/practice/michael/demo/DAOs/EmployeeDao.java
@@ -3,11 +3,14 @@ package com.practice.michael.demo.DAOs;
 import com.practice.michael.demo.DTOs.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface EmployeeDao extends JpaRepository<Employee, Long>{
-    List<Employee> getAllEmployeesWithFirstName(String firstname);
+
+    List<Employee> findByFirstName(String firstname);
 }
